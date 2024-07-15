91dec023821d2d647cf22bf446c73b35537abc3b
Martin Goñi <46424341+Tinch334@users.noreply.github.com>
2024-07-15 19:27:38 -0300

    Delete point.c

--- point.c
+++
@@ -1,8 +0,0 @@
-#include "point.h"
-#include <stdlib.h>
-#include <assert.h>
-
-//Returns 1 if the given points have the same X and Y coordinates.
-int point_equal(const Point p1, const Point p2) {
-    return p1->x == p2->x && p1->y == p2->y;
-}
